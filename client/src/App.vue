e6624399164911a8514c7e9c2449806d629a7c29
Spearhead111 <18101524079@163.com>
2023-03-23 12:04:13 +0800

    路由切换最小化侧边栏判断

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <side-bar v-if="!isHideSidebar" @sendSidebarWidth="changeMarginLeft"></side-bar>
+  <side-bar ref="side_bar" v-show="!isHideSidebar" @sendSidebarWidth="changeMarginLeft"></side-bar>
   <div id="router-view" :style="{ 'margin-left': `${left}px` }">
     <RouterView v-slot="{ Component }">
       <keep-alive :max="10">
@@ -15,17 +15,31 @@ import { computed, onMounted, ref } from '@vue/runtime-core'
 
 import sideBar from '@/components/side-bar/index.vue'
 import useUserStore from '@/stores/modules/user'
+import { watch } from 'vue'
 
+const side_bar = ref()
 const userStore = useUserStore()
 const route = useRoute()
 let left = ref(190)
+// 监听路由的meta.minimizeSidebar变化，调用子组件sideBar的最小化侧边栏方法(子组件sideBar需要暴露这个方法)
+watch(
+  () => route.meta.minimizeSidebar,
+  () => {
+    if (route.meta.minimizeSidebar && side_bar.value && !side_bar.value.minimizeSidebar.value) {
+      side_bar.value.hideSidebar()
+    }
+  }
+)
+/* 根据路由的meta.hideSidebar来判断是否隐藏侧边栏 */
 let isHideSidebar = computed((): boolean => {
   return route.meta.hideSidebar as boolean
 })
+/* 调整右侧路由视窗的margin-left以适应侧边栏变化 */
 const changeMarginLeft = (width: number) => {
   left.value = width
 }
 onMounted(() => {
+  // 解码用户token获取用户基本信息
   userStore.decodeToken()
 })
 </script>
